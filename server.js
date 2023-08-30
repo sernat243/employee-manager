7643ef0301e8c7d226aeca6a95f3a38c5aa30117
Gabriel <sernat243@gmail.com>
2023-08-29 18:53:32 -0600

    created menu prompt and structure for functions

--- server.js
+++ server.js
@@ -1,2 +1,100 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
 
+// connection to mysql - will probably create an .env file later
+const connection = mysql.createConnection({
+    host: 'local_host',
+    user: 'root',
+    password: 'tu3Z@LJlpQ',
+    database: '',
+});
+
+// main menu prompt options
+const mainMenu = [
+    {
+        type: 'list',
+        name: 'menu',
+        message: 'what would you like to do?',
+        choices: ['view departments', 'view roles', 'view employees',
+         'add department', 'add role', 'add employee', 'update employee role', 'exit'],
+    },
+];
+
+//functions
+function viewDepartments() {
+    connection.query('SELECT * FROM departments', (err, results) => {
+        if (err) {
+            console.error('Error fetching departments', err);
+        } else {
+            console.table(results);
+            promptMainMenu();
+        }
+    });
+}
+
+function viewRoles() {
+    connection.query('SELECT * FROM roles', (err, results) => {
+        if (err) {
+            console.error('Error fetching roles', err);
+        } else {
+            console.table(results);
+            promptMainMenu();
+        }
+    });
+}
+
+function viewEmployees() {
+    connection.query('SELECT * FROM employees', (err, results) => {
+        if (err) {
+            console.error('Error fetching employees', err);
+        } else {
+            console.table(results);
+            promptMainMenu();
+        }
+    });
+}
+
+//main menu prompt
+function promptMainMenu() {
+    inquirer.prompt(mainMenu).then((answers) => {
+        switch (answers.menu) {
+            case 'view departments':
+                viewDepartments();
+                break;
+            case 'view roles':
+                viewRoles();
+                break;
+            case 'view employees':
+                viewEmployees();
+                break;
+            case 'add department':
+                addDepartment();
+                break;
+            case 'add role':
+                addRole();
+                break;
+            case 'add employee':
+                addEmployee();
+                break;
+            case 'update employee':
+                updateEmployee();
+                break;
+            case 'exit':
+                connection.end();
+                console.log('goodbye!');
+                break;
+            default:
+                console.log('invalid choice');
+                promptMainMenu();
+        }
+    });
+}
+
+
+
+
+function init() {
+    promptMainMenu();
+}
+
+init();
